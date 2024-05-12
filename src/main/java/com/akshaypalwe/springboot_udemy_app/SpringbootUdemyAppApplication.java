ddcb123885a7645a38e2f2e703f584a7fb70e597
Akshay Palwe <palweakshay20@gmail.com>
2024-05-12 10:28:58 +0530

    Project created

---
+++ src/main/java/com/akshaypalwe/springboot_udemy_app/SpringbootUdemyAppApplication.java
@@ -0,0 +1,13 @@
+package com.akshaypalwe.springboot_udemy_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUdemyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUdemyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshaypalwe/springboot_udemy_app/SpringbootUdemyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshaypalwe.springboot_udemy_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUdemyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
